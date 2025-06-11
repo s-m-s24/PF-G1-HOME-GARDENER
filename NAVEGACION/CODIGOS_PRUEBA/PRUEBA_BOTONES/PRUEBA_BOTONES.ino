039e14ba1f4a78821f00e10ec640e5bf9ad523ff
Sofía Macarena Sama <48045471@est.ort.edu.ar>
2025-06-10 22:25:57 -0300

    Update PRUEBA_BOTONES.ino

--- NAVEGACION/CODIGOS_PRUEBA/PRUEBA_BOTONES/PRUEBA_BOTONES.ino
+++ NAVEGACION/CODIGOS_PRUEBA/PRUEBA_BOTONES/PRUEBA_BOTONES.ino
@@ -1,9 +1,9 @@
-#define BUTTON_UP 2
-#define BUTTON_DOWN 3
-#define BUTTON_OK 4
-#define BUTTON_WATER 5
-#define PUSHED HIGH
-#define N_PUSHED LOW
+#define BUTTON_UP 5
+#define BUTTON_DOWN 4
+#define BUTTON_OK 16
+#define BUTTON_WATER 17
+#define PUSHED LOW
+#define N_PUSHED HIGH
 bool ReadButtonUp = N_PUSHED;
 bool ReadButtonDown = N_PUSHED;
 bool ReadButtonOk = N_PUSHED;
@@ -11,34 +11,43 @@ bool ReadButtonWater = N_PUSHED;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BUTTON_UP,INPUT_PULLUP);
+  pinMode(BUTTON_DOWN,INPUT_PULLUP);
+  pinMode(BUTTON_OK,INPUT_PULLUP);
+  pinMode(BUTTON_WATER,INPUT_PULLUP);
 }
 
 void loop() {
   ReadButtonUp = digitalRead(BUTTON_UP);
+  Serial.print("UP: ");
   if (ReadButtonUp == PUSHED) {
-    Serial.println("PUSHED");
+    Serial.print("PUSHED");
   } else {
-    Serial.println("NOT PUSHED");
+    Serial.print("NOT PUSHED");
   }
 
   ReadButtonDown = digitalRead(BUTTON_DOWN);
+  Serial.print(" | DOWN: ");
   if (ReadButtonDown == PUSHED) {
-    Serial.println("PUSHED");
+    Serial.print("PUSHED");
   } else {
-    Serial.println("NOT PUSHED");
+    Serial.print("NOT PUSHED");
   }
 
   ReadButtonOk = digitalRead(BUTTON_OK);
+  Serial.print(" | OK: ");
   if (ReadButtonOk == PUSHED) {
-    Serial.println("PUSHED");
+    Serial.print("PUSHED");
   } else {
-    Serial.println("NOT PUSHED");
+    Serial.print("NOT PUSHED");
   }
 
   ReadButtonWater = digitalRead(BUTTON_WATER);
+  Serial.print(" | WATER: ");
   if (ReadButtonWater == PUSHED) {
     Serial.println("PUSHED");
   } else {
     Serial.println("NOT PUSHED");
   }
+  delay(500);
 }

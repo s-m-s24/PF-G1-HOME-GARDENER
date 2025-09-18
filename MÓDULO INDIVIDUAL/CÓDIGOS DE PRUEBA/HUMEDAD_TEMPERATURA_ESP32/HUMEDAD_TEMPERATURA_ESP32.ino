f2c884a07984e70b038812960407f0aa6a161377
evswhitte <evamorenocava@gmail.com>
2025-09-18 14:22:17 -0300

    ayuda

--- MÓDULO INDIVIDUAL/CÓDIGOS DE PRUEBA/HUMEDAD_TEMPERATURA_ESP32/HUMEDAD_TEMPERATURA_ESP32.ino
+++ MÓDULO INDIVIDUAL/CÓDIGOS DE PRUEBA/HUMEDAD_TEMPERATURA_ESP32/HUMEDAD_TEMPERATURA_ESP32.ino
@@ -1,23 +1,33 @@
 #include <DHT.h>
 
-#define DHTPIN 4     // Pin donde está conectado el sensor
-#define DHTTYPE DHT22 
+//#define DHTPIN 4     // Pin donde está conectado el sensor
+//#define DHTTYPE DHT22
+#define PINLED 2
 
 #define PIN_HUMEDAD 36
 
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
+  Serial.begin(115200);
+  //dht.begin();
   pinMode(PIN_HUMEDAD, INPUT);
+  pinMode(PINLED, OUTPUT);
 }
 void loop() {
   delay(2000);
   int lecturaHumedad = analogRead(PIN_HUMEDAD);
-  float temperatura = dht.readTemperature(); 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.print(" *C || Humedad: ");
-  Serial.println(lecturaHumedad); //lectura analógica
+  //float temperatura = dht.readTemperature();
+
+  if (lecturaHumedad < 3000) {
+    digitalWrite(PINLED, HIGH);
+
+  } else {
+    digitalWrite(PINLED, LOW);
+  }
+
+  //Serial.print("Temperatura: ");
+  //Serial.print(temperatura);
+  Serial.print("Humedad: ");
+  Serial.println(lecturaHumedad);  //lectura analógica
 }
